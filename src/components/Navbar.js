21ded15acacad5494e5068be2c0862bc2fd145b2
hunnyUnicorn <hunnyUnicorn1@gmail.com>
2024-06-27 02:03:44 +0200

    update the header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -37,7 +37,7 @@ function NavBar() {
     >
       <Container>
         <Navbar.Brand href="#" target="_blank">
-          <MdDeveloperMode style={{ marginBottom: "2px" }} /> DANIELANDREILLC
+          <MdDeveloperMode style={{ marginBottom: "2px" }} /> DANIEL ANDREI
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
